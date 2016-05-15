a6b2d59239af260fbe91c8b1fc544cba79fe7468
Exaltia <ludovic.dsj@protheine.com>
2016-05-16 01:10:21 +0200

    Code cleaning, adding comments, dns modification is working

--- zonecontent.py
+++ zonecontent.py
@@ -6,6 +6,7 @@ import os
 from easyzone import easyzone
 from Cookie import SimpleCookie
 form = cgi.FieldStorage()
+#form is Dnsmanager.py
 dnsname = form.getlist("dname")
 dnsname = str(dnsname)
 dnsname = dnsname.strip("[']")
@@ -18,6 +19,7 @@ try:
 	filename = "/var/cache/bind/%s" % dnsname
 	f = easyzone.zone_from_file(zonename, filename)
 except :
+	#Todo : Make a real error message and a real error handling
 	print "content-type:text/html"
 	print
 	print '<html>'
@@ -49,7 +51,7 @@ nsrecords = f.root.records('NS').items
 tablerecords = ''
 i = 0
 while (i < NbrRecords):
-	tablerecords = nsrecords[i]
+	tablerecords = nsrecords[i] #Add the required code to allow modification
 	print """<input type="text" id="Autre" name="6" value="%s"><br>""" % (tablerecords)
 	i = i +1
 #Displaying all MX records
@@ -57,10 +59,9 @@ NbrRecords = len(f.root.records('MX').items)
 mxrecords = f.root.records('MX').items
 i = 0
 while (i < NbrRecords):
-	print mxrecords
 	listrecords = mxrecords[i]
 	Nbrlistrecords = len(listrecords)
-	print Nbrlistrecords
+	#Add the required code to allow modification
 	print """<input type="text" id="Autre" name="7" value="%s"><input type="text" id="Autre" name="8" value="%s"><br>""" % (listrecords)
 	i = i + 1
 #Displaying all A records
@@ -68,12 +69,11 @@ i = 0
 ii = 0
 allnames = list(f.names)
 zonenamelen = len(allnames)
-print zonenamelen
+separator = ','
 while ( i < zonenamelen):
 	currentzonename = allnames[i]
 	print "<br>"
 	print currentzonename
-	#NbrRecordsperNames = len(f.names[currentzonename].records('A').items)
 	cnames = f.names[currentzonename].records('CNAME')
 	anames = f.names[currentzonename].records('A')
 	aaaanames = f.names[currentzonename].records('AAAA')
@@ -81,28 +81,26 @@ while ( i < zonenamelen):
 	if cnames:
 		strcnames = str(cnames.items)
 		strcnames = strcnames.strip("[']")
-		print """<input type="text" id="Autre" name="%s" value="%s"><input type="submit" name ="modifcname" value="Modifier"><input type="submit" value="Supprimer">""" % (i, strcnames) 
+		print """<input type="text" id="Autre" name="%s" value="%s"><input type="radio" name="radiorecordbutton" value="%s">""" % (i, strcnames,fqdna) 
 	elif anames:
 		anameslen = len (anames.items)
 		if anameslen > 1:
+			#handle if there is more than 1 A record for a single fqdn
 			while ii < anameslen:
 				stranames = str(anames.items[ii])
 				stranames = stranames.strip("[']")
-				separator = ','
 				idandvalue = ''
 				stri = ''
 				stri = str(i)
 				idandvalue = stri + separator + stranames
 				fqdna = str(currentzonename) + separator + stranames
 				print """<input type="text" id="Autre" name="%s" value="%s"><input type="radio" name="radiorecordbutton" value="%s">""" % (stranames, stranames, fqdna)
-				print '<br>' + fqdna
 				ii = ii + 1
 		if anameslen < 2:
 			stranames = str(anames.items)
 			stranames = stranames.strip("[']")
 			fqdna = str(currentzonename) + separator + stranames
 			print """<input type="text" id="Autre" name="%s" value="%s"><input type="radio" name="radiorecordbutton" value="%s">""" % (stranames, stranames, fqdna)
-			print '<br>' + fqdna
 	elif aaaanames:
 		ii = 0
 		print '<br> ii Value is : '
@@ -111,7 +109,7 @@ while ( i < zonenamelen):
 		while ii < anameslen:
 			straaaanames = str(aaaanames.items)
 			straaaanames = straaaanames.strip("[']")
-			print """<input type="text" id="Autre" name="Mmmonamemanualinput" value="%s"><input type="submit" value="Modifier">""" % (straaaanames)
+			print """<input type="text" id="Autre" name="Mmmonamemanualinput" value="%s"><input type="radio" name="radiorecordbutton" value="%s">""" % (straaaanames, fqdna)
 			ii = ii +1
 	elif txtnames:
 		ii = 0
@@ -119,27 +117,11 @@ while ( i < zonenamelen):
 		while ii < anameslen:
 			strtxtnames = str(txtnames.items)
 			strtxtnames = strtxtnames.strip("[']")
-			print """<input type="text" id="Autre" name="Mmmonamemanualinput" value="%s"><input type="submit" value="Modifier">""" % (strtxtnames)
+			print """<input type="text" id="Autre" name="Mmmonamemanualinput" value="%s"><input type="radio" name="radiorecordbutton" value="%s">""" % (strtxtnames, fqdna)
 			ii = ii + 1
 	else:
 		print "Error, the zone has neither a CNAME, A or TXT records"
-	# print "Nombre "
-	# print NbrRecordsperNames
-	#arecords = f.names[currentzonename].records('A').items
-	#print type(arecords)
-	#print """<input type="text" id="Autre" name="Mmmonamemanualinput" value="%s"><input type="text" id="Autre" name="Mmmonamemanualinput" value="%s"><br>""" % (arecords)
-	print i
 	i = i + 1
 print '<br>'
-#if 'grosboutonrouge1'
-#print '<form name="formulaire" action="zoneitemchange.py" method="POST">'
 print '<input type="submit" name="action" value="Modifier">'
 print '<input type="submit" name= "action" value="Supprimer">'
-# if "Modifier" in form:
-	# print '<form name="formulaire" action="zoneitemchange.py" method="POST">'
-# if "Supprimer" in form:
-	# print '<form name="formulaire" action="zoneitemdelete.py" method="POST">'
-# else:
-	# print 'thefuk'
-print '<hr>'
-print C
--- zoneitemchange.py
+++ zoneitemchange.py
@@ -5,58 +5,51 @@ import cgitb
 import os
 from easyzone import easyzone
 from Cookie import SimpleCookie
+from easyzone.zone_check import ZoneCheck
 # import zonecontent This is a a reminder TODO refactoring everything for beter code and use variable import
 C = SimpleCookie((os.environ["HTTP_COOKIE"]))
 form = cgi.FieldStorage()
-var = []
+#form is zonecontent.py
+var = [] # Check if this var is still useful
 action = []
+#check if the following for is still useful, i doubt it
 for item in form.getlist("htmlanames"):
 	var.append(item)
 print "content-type:text/html"
-print C
+print C #Adding cookie inside http headers
 print
+#get required info from form in order to process zone changes
 radio = form.getvalue('radiorecordbutton')
 fqdna = form.getvalue('radiorecordbutton')
 fqdna = fqdna.split(',')
 CurrentDomainName = str(C)
 CurrentDomainName = CurrentDomainName.split('=')
-#print CurrentDomainName
-print '<hr>'
-print C['Current-domain-name']
 for item in form.getlist('action'):
 	action.append(item)
-print radio
-print '<hr>'
 action = str(action)
 action = action.strip("[']")
-print radio
-print action
-print '<br> fqdn is: '
-print fqdna[0]
-print '<br>'
 fqdn = str(fqdna[0])
 arecord = str(fqdna[1])
-print fqdn + '<br>'
-print arecord + '<br>'
-print '<hr>'
-if action == 'Modifier':
-	# currentrecord = radio
-	# newrecord = form.getvalue(radio)
-	# print currentrecord 
-	# print '<br>' 
-	# if type(newrecord) is list:
-		# wantedrecord = newrecord[0]
-		# print newrecord
-		# print wantedrecord
+if action == 'Modifier': # easyzone doesn't really have a modify function, forcing to do add then delete
+	currentrecord = arecord
+	newrecord = form.getvalue(arecord) 
+	print '<br>' 
+	if type(newrecord) is list:
+		wantedrecord = newrecord[0]
+	else:
+		wantedrecord = newrecord
 	try:
 		zonename = str(CurrentDomainName[1])
-		print zonename
 		filename = "/var/cache/bind/%s" % zonename
 		f = easyzone.zone_from_file(zonename, filename)
-		f.names[fqdn].records('A').add(arecord)
+		f.names[fqdn].records('A').add(wantedrecord)
+		f.names[fqdn].records('A').delete(arecord)
 		test = f.names[fqdn].records('A').items
+		test = str(test)
+		print '<br> test is ' + test
 	except:
 		print 'Foiré!'
+		result = 'NOK' # easyzone check the record validity even before passing it to the isValid function
 elif action == 'Supprimer':
 	try:
 		zonename = str(CurrentDomainName[1])
@@ -69,9 +62,19 @@ elif action == 'Supprimer':
 		print test
 	except:
 		print 'Couille dans le potage'
+		result = 'NOK'
 else:
 	print 'Error'
-# print zonecontent.dnsname This is a a reminder TODO refactoring everything for beter code and use variable import
-# print '<br>'
-#print C
-#print action
+#The destination directory (example : /var/cache/bind) must be owned by www-data:bind
+tempfilesuffix = '.tmp'
+tempfile = filename + tempfilesuffix
+f.save (tempfile, autoserial=True)
+dnscheck = ZoneCheck(checkzone='/usr/sbin/named-checkzone')
+valid = dnscheck.isValid(zonename, tempfile)
+if valid is True and result is not 'NOK': #If we don't check for NOK, easyzone write anyways with
+	#	 the old valid info, procing the error message from 
+	#the failed f function then the ok one because the old zone is still valid when 
+	#rewrited
+	print 'ok!'
+	f.save (filename, autoserial=True)
+	os.remove(tempfile)
